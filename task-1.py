9d9e8afd49f81ff3070880849bc6808042d9d59a
akrom_17a <149670082+akrom17ua@users.noreply.github.com>
2025-10-18 00:47:13 +0500

    Create task-1.py

---
+++ task-1.py
@@ -0,0 +1,9 @@
+1. Password Strength Checker
+
+Ask the user to enter a password.
+
+Verify the input is a string (use try/except to catch non-string types).
+
+If the password’s length is 6 or more and it contains "@", print "Strong password".
+
+Otherwise, print "Weak password".
